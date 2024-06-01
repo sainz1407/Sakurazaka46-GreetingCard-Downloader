64cc984476f5af97d03849325fdbc68010c0ebce
Sainz <80474991+sainz1407@users.noreply.github.com>
2024-06-01 17:38:15 +0700

    Update sakuGreeting.py

--- sakuGreeting.py
+++ sakuGreeting.py
@@ -2,12 +2,7 @@ from bs4 import BeautifulSoup as bs
 import requests
 import urllib.request
 import os
-import io
-import sys
-import warnings
-import json
 import re
-warnings.filterwarnings("ignore")
 
 def get_html(url):
     req = urllib.request.Request(url)
@@ -16,37 +11,37 @@ def get_html(url):
     html = response.read()
     return html
 
-def downloadImg(memName, srcLink, savedir):
+def download_image(memName, src_link, savedir):
     if not os.path.isdir(savedir):
         os.makedirs(savedir)
-    tmp = requests.get(srcLink)
-    with open(os.path.join(savedir, memName + '.jpg'), 'wb') as f:
+
+    filename = os.path.join(savedir, memName + '.jpg')
+    src_link = re.sub(r'/\d+_\d+_\d+\.jpg$', '.jpg', src_link)
+    tmp = requests.get("https://sakurazaka46.com/" + src_link)
+
+    with open(filename, 'wb') as f:
         f.write(tmp.content)
-    return "OK"
+
+def process_member(memNum):
+    url = f"https://sakurazaka46.com/s/s46/artist/{memNum}?ima=0000"
+    html = get_html(url)
+    res = bs(html, 'html.parser')
+
+    memName = res.find(class_="name").text.strip()
+    memPic_src = res.find(class_="ph").find('img')['src']
+    memCard_src = res.find(class_="part-card").find('img')['src']
+    PHOTO = res.find(class_="part-cimg")
+
+    download_image(memName, memPic_src, "Pic")
+    download_image(memName, memCard_src, "GreetingCard")
+
+    if PHOTO:
+        photo_src = PHOTO.find('img')['src']
+        download_image(memName, photo_src, "GreetingPHOTO")
+
+    print(memName)
 
 memList = ["03", "06", "08", "43", "53", "54", "55", "56", "45", "46", "47", "57", "48", "50", "58", "51", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69"]
 
 for memNum in memList:
-    url = "https://sakurazaka46.com/s/s46/artist/" + str(memNum) + "?ima=0000"
-    html = get_html(url)
-    res = bs(html, 'html.parser')
-    memPic = "https://sakurazaka46.com/" + res.find(class_="ph").find('img')['src']
-    memName = res.find(class_="name").text.strip()
-    PHOTO = res.find(class_="part-cimg")
-    memCard_img_tag = res.find(class_="part-card").find('img')
-    memCard_src = memCard_img_tag['src']
-
-    memCard_src = re.sub(r'/\d+_\d+_\d+\.jpg$', '.jpg', memCard_src)
-    memCard = "https://sakurazaka46.com/" + memCard_src
-    if PHOTO is None:
-        downloadImg(memName, memPic, "Pic")
-        downloadImg(memName, memCard, "GreetingCard")
-    else:
-        photo_img_tag = PHOTO.find('img')
-        photo_src = photo_img_tag['src']
-        photo_src = re.sub(r'/\d+_\d+_\d+\.jpg$', '.jpg', photo_src)
-        Photo = "https://sakurazaka46.com/" + photo_src
-        downloadImg(memName, memPic, "Pic")
-        downloadImg(memName, memCard, "GreetingCard")
-        downloadImg(memName, Photo, "GreetingPHOTO")
-    print(memName)
+    process_member(memNum)
